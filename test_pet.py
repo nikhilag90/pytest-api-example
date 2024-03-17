7d77ea77c1994ee2f2c6d0cb5cae85070ea5e482
Anunikhila Gubba <nikhila.g2015@gmail.com>
2024-03-18 00:29:41 +0530

    Added/Fixed tests based on the instructions

--- test_pet.py
+++ test_pet.py
@@ -9,6 +9,8 @@ TODO: Finish this test by...
 1) Troubleshooting and fixing the test failure
 The purpose of this test is to validate the response matches the expected schema defined in schemas.py
 '''
+
+
 def test_pet_schema():
     test_endpoint = "/pets/1"
 
@@ -17,7 +19,11 @@ def test_pet_schema():
     assert response.status_code == 200
 
     # Validate the response schema against the defined schema in schemas.py
+
+    # Fixed the bug in schema under properties -> name -> type. Pet schema had name.type as integer
+    # instead of expected type of string
     validate(instance=response.json(), schema=schemas.pet)
+
 
 '''
 TODO: Finish this test by...
@@ -26,7 +32,9 @@ TODO: Finish this test by...
 3) Validate the 'status' property in the response is equal to the expected status
 4) Validate the schema for each object in the response
 '''
-@pytest.mark.parametrize("status", [("available")])
+
+
+@pytest.mark.parametrize("status", [("available"), ("sold"), ("pending")])
 def test_find_by_status_200(status):
     test_endpoint = "/pets/findByStatus"
     params = {
@@ -34,13 +42,27 @@ def test_find_by_status_200(status):
     }
 
     response = api_helpers.get_api_data(test_endpoint, params)
-    # TODO...
+
+    # validate the response code
+    assert response.status_code == 200
+    for pet_schema in response.json():
+        pet_status = pet_schema.get('status')
+        # validate the pet status is equal to expected status
+        assert pet_status == status
+        # validate pet schema
+        validate(instance=pet_schema, schema=schemas.pet)
+
 
 '''
 TODO: Finish this test by...
 1) Testing and validating the appropriate 404 response for /pets/{pet_id}
 2) Parameterizing the test for any edge cases
 '''
-def test_get_by_id_404():
-    # TODO...
-    pass
+
+# parameterized to test for edge cases
+@pytest.mark.parametrize("pet_id", [("1"), ("-1"), ("$")])
+def test_get_by_id_404(pet_id):
+    pet_get_by_pet_id = "/pets" + pet_id
+
+    # validated the response status code get_by_pet_id
+    assert api_helpers.get_api_data(pet_get_by_pet_id).status_code == 404
--- test_store.py
+++ test_store.py
@@ -1,3 +1,7 @@
+import json
+import random
+import string
+
 from jsonschema import validate
 import pytest
 import schemas
@@ -12,5 +16,54 @@ TODO: Finish this test by...
 3) Validate the response codes and values
 4) Validate the response message "Order and pet status updated successfully"
 '''
-def test_patch_order_by_id():
-    pass
+
+
+@pytest.fixture
+def create_pet_test_data():
+    def create_request():
+        pet_id = random.randint(1, 99)
+        pet_name = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))
+        pet_type = random.choice(['cat', 'dog', 'fish'])
+        pet_status = random.choice(['available'])
+
+        request_data = {
+            "id": pet_id,
+            "name": pet_name,
+            "type": pet_type,
+            "status": pet_status
+        }
+
+        return json.dumps(request_data)
+
+    return create_request
+
+
+def test_patch_order_by_id(create_pet_test_data):
+    # create pet data
+    create_pets_endpoint = "/pets"
+    pet_request_data = create_pet_test_data()
+    pet_id = json.loads(pet_request_data).get('id')
+
+    create_pets_response = api_helpers.post_api_data(create_pets_endpoint, json.loads(pet_request_data))
+
+    assert create_pets_response.status_code == 201
+    assert create_pets_response.json().get('id') == pet_id
+
+    # create order
+    create_order_endpoint = "/store/order"
+    create_order_request_data = {"pet_id": pet_id}
+    create_order_response = api_helpers.post_api_data(create_order_endpoint,
+                                                      json.loads(json.dumps(create_order_request_data)))
+
+    assert create_order_response.status_code == 201
+
+    # validate order schema
+    validate(instance=create_order_response.json(), schema=schemas.order)
+
+    # update the order with pet status
+    order_id = create_order_response.json().get('id')
+    patch_request_data = {"status": "pending"}
+    create_patch_order_response = api_helpers.patch_api_data(create_order_endpoint + "/" + order_id,
+                                                             json.loads(json.dumps(patch_request_data)))
+    assert create_patch_order_response.status_code == 200
+    assert create_patch_order_response.json().get('message') == "Order and pet status updated successfully"
